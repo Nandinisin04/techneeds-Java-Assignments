4366504dbaf047352ccf1cb392cca7be44fc8b17
Nandinisin04 <144032660+Nandinisin04@users.noreply.github.com>
2023-09-30 01:38:57 +0530

    i answered Q8

---
+++ a1_q8.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Rotate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int nod = 0;  
+        int temp = n; 
+        while (temp != 0){
+            temp /= 10;
+            nod++;
+        }
+        int div = 1;
+        int mult = 1; 
+        for (int i = 1; i <= nod; i++){
+            if (i <= k) { 
+                div *= 10;
+            }
+            else         
+            {
+                mult *= 10;
+            }
+        }
+        int quo = n / div;    
+        int rem = n % div;
+        int r = rem * mult + quo; 
+        System.out.println(r);
+    }
+}
