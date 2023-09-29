b04b8a55e1682151cef62a49f444e60a8a6d5eae
Nandinisin04 <144032660+Nandinisin04@users.noreply.github.com>
2023-09-30 01:14:39 +0530

    i answered Q2

---
+++ a1_q2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Grading {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(" Enter the marks of a student: ");
+        int marks = sc.nextInt();
+        if (marks>90){
+            System.out.println("Excellent");
+        }
+        else if (marks>80 && marks<= 90){
+            System.out.println("Good");
+        }
+        else if (marks>70 && marks<=80){
+            System.out.println("Fair");
+        }
+        else if (marks>60 && marks<=70){
+            System.out.println("Meets expectation");
+        }
+        else{
+            System.out.println("below average ");
+        }
+    }
+}
